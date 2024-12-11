da22ed2172fd0d8b07ed63ae09c933761051168d
v-kuper <vkuper7@gmail.com>
2024-12-11 23:39:47 +0300

    feat: add location detail view

---
+++ DubDubGrub/LocationDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDetailView.swift
+//  DubDubGrub
+//
+//  Created by Vitali Kupratsevich on 11.12.24.
+//
+
+import SwiftUI
+
+struct LocationDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDetailView()
+}
